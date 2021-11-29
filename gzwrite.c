b99a38bfaa644b07bf70afe6ac9bf78ccfcb931c
Simon Rozman <simon@rozman.si>
2021-11-29 11:28:09 +0100

    Address MSVC C4267 warnings
    Mostly, the warnings were false positives and were silenced using
    typecasting.
    
    However, gzputs() did allow integer overflow when input string is longer
    than 2 Gi characters.
    
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- gzwrite.c
+++ gzwrite.c
@@ -209,7 +209,7 @@ local z_size_t gz_write(state, buf, len)
                               state->in);
             copy = state->size - have;
             if (copy > len)
-                copy = len;
+                copy = (unsigned)len;
             memcpy(state->in + have, buf, copy);
             state->strm.avail_in += copy;
             state->x.pos += copy;
@@ -229,7 +229,7 @@ local z_size_t gz_write(state, buf, len)
         do {
             unsigned n = (unsigned)-1;
             if (n > len)
-                n = len;
+                n = (unsigned)len;
             state->strm.avail_in = n;
             state->x.pos += n;
             if (gz_comp(state, Z_NO_FLUSH) == -1)
@@ -368,7 +368,12 @@ int ZEXPORT gzputs(file, str)
 
     /* write string */
     len = strlen(str);
-    ret = gz_write(state, str, len);
+    if ((int)len < 0) {
+        gz_error(state, Z_STREAM_ERROR, "strlen does not fit in an int");
+        return -1;
+    }
+
+    ret = (int)gz_write(state, str, len);
     return ret == 0 && len != 0 ? -1 : ret;
 }
 
